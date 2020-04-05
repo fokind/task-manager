7de24a7382e686bf3cbb7a47e9a82926f34124d2
Dmitry Fokin <fokind1@yandex.ru>
2020-04-05 17:38:55 +0300

    редактирование задачи

--- src/webapp/controller/Kanban.controller.js
+++ src/webapp/controller/Kanban.controller.js
@@ -1,153 +1,248 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function (
-    Controller,
-    MessageToast
-) {
-    "use strict";
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/m/Dialog",
+        "sap/m/VBox",
+        "sap/m/TextArea",
+        "sap/m/Button",
+        "sap/m/library",
+    ],
+    function (
+        Controller,
+        MessageToast,
+        Dialog,
+        VBox,
+        TextArea,
+        Button,
+        mobileLibrary
+    ) {
+        "use strict";
 
-    return Controller.extend("fokind.kanban.controller.Kanban", {
-        onInit: function () {
-            var oComponent = this.getOwnerComponent();
-            oComponent
-                .getRouter()
-                .getRoute("kanban")
-                .attachMatched(this._onRouteMatched, this);
-            this.getView().addStyleClass(oComponent.getContentDensityClass());
-        },
+        var ButtonType = mobileLibrary.ButtonType;
 
-        _onRouteMatched: function (oEvent) {
-            var oView = this.getView();
-            var sId = oEvent.getParameter("arguments").id;
-            var sPath = "/Projects('" + sId + "')";
-            oView.bindObject({
-                path: sPath,
-                parameters: {
-                    $expand: "States($expand=Tasks)",
-                },
-            });
+        return Controller.extend("fokind.kanban.controller.Kanban", {
+            onInit: function () {
+                var oComponent = this.getOwnerComponent();
+                oComponent
+                    .getRouter()
+                    .getRoute("kanban")
+                    .attachMatched(this._onRouteMatched, this);
+                this.getView().addStyleClass(
+                    oComponent.getContentDensityClass()
+                );
+            },
 
-            var oContext = oView.getBindingContext();
-            var oModel = oView.getModel();
-            var oStatesBinding = oModel.bindList("States", oContext);
-            oContext.requestObject().then(function () {
-                var oDraft = {
-                    States: oStatesBinding
-                        .getContexts()
-                        .map(function (oStateContext) {
-                            return {
-                                _id: oStateContext.getProperty("_id"),
-                                title: oStateContext.getProperty("title"),
-                                Tasks: oModel
-                                    .bindList("Tasks", oStateContext)
-                                    .getContexts()
-                                    .map(function (oTaskContext) {
-                                        return {
-                                            _id: oTaskContext.getProperty(
-                                                "_id"
-                                            ),
-                                            title: oTaskContext.getProperty(
-                                                "title"
-                                            ),
-                                        };
-                                    }),
-                            };
-                        }),
-                };
+            _onRouteMatched: function (oEvent) {
+                var oView = this.getView();
+                var sId = oEvent.getParameter("arguments").id;
+                var sPath = "/Projects('" + sId + "')";
+                oView.bindObject({
+                    path: sPath,
+                    parameters: {
+                        $expand: "States($expand=Tasks)",
+                    },
+                });
 
-                oView.getModel("draft").setData(oDraft);
-            });
-        },
+                var oContext = oView.getBindingContext();
+                var oModel = oView.getModel();
+                var oStatesBinding = oModel.bindList("States", oContext);
+                oContext.requestObject().then(function () {
+                    var oDraft = {
+                        States: oStatesBinding
+                            .getContexts()
+                            .map(function (oStateContext) {
+                                return {
+                                    _id: oStateContext.getProperty("_id"),
+                                    title: oStateContext.getProperty("title"),
+                                    Tasks: oModel
+                                        .bindList("Tasks", oStateContext)
+                                        .getContexts()
+                                        .map(function (oTaskContext) {
+                                            return {
+                                                _id: oTaskContext.getProperty(
+                                                    "_id"
+                                                ),
+                                                title: oTaskContext.getProperty(
+                                                    "title"
+                                                ),
+                                            };
+                                        }),
+                                };
+                            }),
+                    };
 
-        onEditPress: function () {
-            var sId = this.getView().getBindingContext().getProperty("_id");
-            this.getOwnerComponent().getRouter().navTo("projectEdit", {
-                id: sId,
-            });
-        },
+                    oView.getModel("draft").setData(oDraft);
+                });
+            },
 
-        onAddPress: function () {
-            // var sProjectId = this.getView().getBindingContext().getProperty("_id");
-            // var oList = this.byId("tasks");
-            // var oBinding = oList.getBinding("items");
-            // var oContext = oBinding.create({
-            //     title: "Task",
-            //     projectId: sProjectId
-            // });
-            // oContext.created().then(function() {
-            //     oContext.getModel().refresh();
-            // });
-        },
+            onEditPress: function () {
+                var sId = this.getView().getBindingContext().getProperty("_id");
+                this.getOwnerComponent().getRouter().navTo("projectEdit", {
+                    id: sId,
+                });
+            },
 
-        _setTaskStatePromise: function (sTaskId, sStateId) {
-            return $.ajax({
-                async: true,
-                url: "/odata/Tasks('" + sTaskId + "')", // TODO получать путь из модели
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                data: JSON.stringify({
-                    stateId: sStateId,
-                }),
-            });
-        },
+            onAddPress: function () {
+                // var sProjectId = this.getView().getBindingContext().getProperty("_id");
+                // var oList = this.byId("tasks");
+                // var oBinding = oList.getBinding("items");
+                // var oContext = oBinding.create({
+                //     title: "Task",
+                //     projectId: sProjectId
+                // });
+                // oContext.created().then(function() {
+                //     oContext.getModel().refresh();
+                // });
+            },
 
-        onDrop: function (oEvent) {
-            var oDraggedItem = oEvent
-                .getParameter("draggedControl")
-                .getBindingContext("draft")
-                .getObject();
+            _saveTaskPromise: function (sTaskId, oDelta) {
+                return $.ajax({
+                    async: true,
+                    url: "/odata/Tasks('" + sTaskId + "')", // TODO получать путь из модели
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    data: JSON.stringify(oDelta),
+                });
+            },
 
-            // удалить из исходного
-            var oDraggedList = oEvent
-                .getParameter("draggedControl")
-                .getParent()
-                .getBindingContext("draft")
-                .getProperty("Tasks");
-            var iDraggedIndex = oDraggedList.indexOf(oDraggedItem);
-            oDraggedList.splice(iDraggedIndex, 1);
+            _openDialog: function (sTitle, fnCallback) {
+                var oDialog = new Dialog({
+                    title: "Title",
+                    content: [
+                        new VBox({
+                            items: [
+                                new TextArea("title", {
+                                    width: "100%",
+                                    value: sTitle,
+                                    placeholder: "Input something...",
+                                }),
+                            ],
+                        }).addStyleClass("sapUiTinyMargin"),
+                    ],
+                    beginButton: new Button({
+                        type: ButtonType.Emphasized,
+                        text: "OK",
+                        press: function () {
+                            var sText = sap.ui
+                                .getCore()
+                                .byId("title")
+                                .getValue();
+                            fnCallback(null, sText);
+                            oDialog.close();
+                        },
+                    }),
+                    endButton: new Button({
+                        text: "Cancel",
+                        press: function () {
+                            oDialog.close();
+                        },
+                    }),
+                    afterClose: function () {
+                        oDialog.destroy();
+                    },
+                });
 
-            // добавить в целевой
-            var sDropPosition = oEvent.getParameter("dropPosition");
-            var oDroppedControl;
-            var oDroppedList;
+                oDialog.open();
+            },
 
-            if (sDropPosition === "On") {
-                oDroppedControl = oEvent.getParameter("droppedControl");
-                oDroppedList = oDroppedControl
-                    .getBindingContext("draft")
-                    .getProperty("Tasks");
-                oDroppedList.push(oDraggedItem);
-            } else {
-                var oDroppedItem = oEvent
-                    .getParameter("droppedControl")
+            onDetailPress: function (oEvent) {
+                var oBindingContext = oEvent
+                    .getSource()
+                    .getBindingContext("draft");
+                var sPath = oBindingContext.getPath();
+                var sTitle = oBindingContext.getProperty("title");
+
+                this._openDialog(
+                    sTitle,
+                    function (oError, sText) {
+                        oBindingContext
+                            .getModel()
+                            .setProperty(sPath + "/title", sText);
+
+                        var sTaskId = oBindingContext.getProperty("_id");
+                        var sTitle = oBindingContext.getProperty("title");
+
+                        var oDelta = {
+                            title: sTitle,
+                        };
+
+                        this._saveTaskPromise(sTaskId, oDelta).then(
+                            function () {
+                                MessageToast.show(
+                                    "Изменения успешно сохранены."
+                                );
+                            }
+                        );
+                    }.bind(this)
+                );
+            },
+
+            onDrop: function (oEvent) {
+                var oDraggedItem = oEvent
+                    .getParameter("draggedControl")
                     .getBindingContext("draft")
                     .getObject();
-                oDroppedControl = oEvent
-                    .getParameter("droppedControl")
-                    .getParent();
-                oDroppedList = oDroppedControl
+
+                // удалить из исходного
+                var oDraggedList = oEvent
+                    .getParameter("draggedControl")
+                    .getParent()
                     .getBindingContext("draft")
                     .getProperty("Tasks");
-                var iDroppedIndex =
-                    oDroppedList.indexOf(oDroppedItem) +
-                    (sDropPosition === "After" ? 1 : 0);
-                oDroppedList.splice(iDroppedIndex, 0, oDraggedItem);
-            }
+                var iDraggedIndex = oDraggedList.indexOf(oDraggedItem);
+                oDraggedList.splice(iDraggedIndex, 1);
 
-            this.getView().getModel("draft").refresh();
+                // добавить в целевой
+                var sDropPosition = oEvent.getParameter("dropPosition");
+                var oDroppedControl;
+                var oDroppedList;
 
-            // сохранить изменения
-            var sTaskId = oDraggedItem._id;
-            var sStateId = oDroppedControl
-                .getBindingContext("draft")
-                .getProperty("_id");
-            this._setTaskStatePromise(sTaskId, sStateId).then(function () {
-                MessageToast.show("Изменения успешно сохранены.");
-            });
-        },
+                if (sDropPosition === "On") {
+                    oDroppedControl = oEvent.getParameter("droppedControl");
+                    oDroppedList = oDroppedControl
+                        .getBindingContext("draft")
+                        .getProperty("Tasks");
+                    oDroppedList.push(oDraggedItem);
+                } else {
+                    var oDroppedItem = oEvent
+                        .getParameter("droppedControl")
+                        .getBindingContext("draft")
+                        .getObject();
+                    oDroppedControl = oEvent
+                        .getParameter("droppedControl")
+                        .getParent();
+                    oDroppedList = oDroppedControl
+                        .getBindingContext("draft")
+                        .getProperty("Tasks");
+                    var iDroppedIndex =
+                        oDroppedList.indexOf(oDroppedItem) +
+                        (sDropPosition === "After" ? 1 : 0);
+                    oDroppedList.splice(iDroppedIndex, 0, oDraggedItem);
+                }
 
-        onBackPress: function () {
-            this.getOwnerComponent().getRouter().navTo("projects");
-        },
-    });
-});
+                this.getView().getModel("draft").refresh();
+
+                // сохранить изменения
+                var sTaskId = oDraggedItem._id;
+                var sStateId = oDroppedControl
+                    .getBindingContext("draft")
+                    .getProperty("_id");
+
+                var oDelta = {
+                    stateId: sStateId,
+                };
+
+                this._saveTaskPromise(sTaskId, oDelta).then(function () {
+                    MessageToast.show("Изменения успешно сохранены.");
+                });
+            },
+
+            onBackPress: function () {
+                this.getOwnerComponent().getRouter().navTo("projects");
+            },
+        });
+    }
+);
